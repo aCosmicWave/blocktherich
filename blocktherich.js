32c20eec099e0930332a3ce004995f4b570424f2
aCosmicWave <118835026+aCosmicWave@users.noreply.github.com>
2023-07-26 22:31:05 -0500

    starting to tag nodes, adding logo to nodes, and making logo interactable

--- blocktherich.js
+++ blocktherich.js
@@ -7,7 +7,7 @@ const blackList = {
     ellison: ["larry ellison", "lawrence joseph ellison"],
     gates: ["bill gates", "william henry gates iii"],
     kanye: ["kanye", "kanye west", "ye west", "kanye omari west"],
-    musk: ["musk", "elon musk", "elon reeve musk"],
+    musk: ["elon", "musk", "elon musk", "elon reeve musk"],
     page: ["larry page", "lawrence edward page"],
     trump: ["trump", "donald trump", "donald j. trump", "donald john trump"],
     zuck: ["zuck", "zuckerberg", "mark zuckerberg", "mark elliot zuckerberg"]
@@ -18,7 +18,6 @@ browser.storage.local.get("status")
         let statusValue = result.status;
         if (statusValue == null || statusValue == true) {
             createObserver();
-
             if (isRich(document.body.textContent)) {
                 findRichNode(document.body);
             }
@@ -27,33 +26,36 @@ browser.storage.local.get("status")
     );
 
 function findRichNode(node) {
+    if (node.parentNode.hasAttribute("btr-rich-node")) {
+        return;
+    }
     if (node.hasChildNodes()) {
         node.childNodes.forEach(element => {
             if (node.nodeName = "A") {
                 if (node.href != undefined && node.href != '') {
                     if (isRichUrl(node.href)) {
-                        blurNode(node);
+                        processNode(node);
                         return;
                     }
                 }
             }
-            findRichNode(element)
+            findRichNode(element);
         })
     } else if (node.nodeType === Node.TEXT_NODE) {
         if (node.parentNode && node.parentNode.nodeName === 'TEXTAREA') {
             return;
         } else if (isRich(node.textContent)) {
-            blurNode(node.parentNode);
+            processNode(node);
         }
     } else if (node.nodeName = "IMG") {
         if (node.src != undefined && node.src != '') {
             if (isRichUrl(node.src)) {
-                blurNode(node);
+                processNode(node);
             }
         }
         if (node.alt != undefined && node.alt != '') {
             if (isRich(node.alt)) {
-                blurNode(node);
+                processNode(node);
             }
 
         }
@@ -87,12 +89,53 @@ function isRichUrl(url) {
     return hasMatch;
 }
 
-function blurNode(node) {
+function processNode(node) {
+    let parentNode = node.parentNode;
+    if (parentNode !== document.body) {
+        addSelectorToNode(parentNode);
+        addLogoToNode(parentNode);
+        addBlurToNode(parentNode);
+    }
+}
+
+function addLogoToNode(node) {
+    let img = document.createElement('img');
+    img.addEventListener("click", function (event) { handleLogoClick(this); });
+    img.classList.add('btr-logo');
+    img.src = browser.extension.getURL("icons/blocktherich-on-96.png");
+    img.style.transition = "1s ease";
+    node.insertAdjacentElement("beforebegin", img);
+}
+
+function addBlurToNode(node) {
+    node.style.filter = "blur(1.5rem)";
+    node.style.transition = "1s ease";
+    node.addEventListener("mouseenter", function (event) { removeBlur(this); });
+    node.addEventListener("mouseleave", function (event) { addBlur2(this); });
+}
+
+function addBlur2(node) {
+    node.previousSibling.style.opacity = "1";
     node.style.filter = "blur(1.5rem)";
 }
 
-function createObserver() {
+function removeBlur(node) {
+    node.previousSibling.style.opacity = "0";
+    node.style.filter = "";
+}
 
+function addSelectorToNode(node) {
+    if (!node.hasAttribute("btr-rich-node")) {
+        node.setAttribute("btr-rich-node", true);
+    }
+}
+
+function handleLogoClick(node) {
+    node.nextSibling.style.filter = "";
+    node.style.display = "none";
+}
+
+function createObserver() {
     const observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
             if (mutation.addedNodes && mutation.addedNodes.length > 0) {
